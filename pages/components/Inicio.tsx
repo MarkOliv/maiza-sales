005ab18b2acfbdcdb1c35b3d00c57ca7e270fe35
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-05-15 09:34:33 -0300

    init project

---
+++ pages/components/Inicio.tsx
@@ -0,0 +1,8 @@
+// @flow
+import * as React from "react";
+type Props = {};
+const Inicio = (props: Props) => {
+  return <div className="container mx-auto ">teste</div>;
+};
+
+export default Inicio;
