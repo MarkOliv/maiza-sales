171a3c9407eb0836f665ae1b2960bc9e10edd8cb
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-05-17 10:38:14 -0300

    upload part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Inicio from "./components/Inicio";
 import decorativotop from "./assets/decorativo-top.png";
+import Sobre from "./components/Sobre";
 
 export default function Home() {
   return (
     <div>
       <Inicio />
+      <Sobre />
     </div>
   );
 }
