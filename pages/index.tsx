ed6544af138ae87d0deae8f63bb88906e173546f
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-05-17 19:12:29 -0300

    add part3 garantia

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import styles from "../styles/Home.module.css";
 import Inicio from "./components/Inicio";
 import decorativotop from "./assets/decorativo-top.png";
 import Sobre from "./components/Sobre";
+import Garantia from "./components/Garantia";
 
 export default function Home() {
   return (
     <div>
       <Inicio />
       <Sobre />
+      <Garantia />
     </div>
   );
 }
