875face973e3ab7c5247ff2dbcb23c8f92c597cb
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-07-04 19:32:42 -0300

    add metatag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,12 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <head>
+        <meta
+          name="facebook-domain-verification"
+          content="7np134rsdkaz2j7dmjcwu2twcep71r"
+        />
+      </head>
       <Component {...pageProps} />
     </div>
   );
